8b470010d71577015a2093ffdaae0dd900cb1cbd
Ömer Faruk Yıldırım <106185278+omerfyildirim@users.noreply.github.com>
2024-01-29 13:44:18 +0200

    Add files via upload

---
+++ src/main/java/org/example/Translator.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class Translator {
+
+    public static void main(String[] args) throws IOException {
+        String text = "Merhaba Dünya!";
+        //Translated text to: ....
+        System.out.println("Translated text: " + translate("tr", "kr", text));
+    }
+
+    private static String translate(String langFrom, String langTo, String text) throws IOException {
+        // INSERT YOU URL HERE
+        String urlStr = "https://script.google.com/macros/s/AKfycbwCqBHDnh30IrCKRBddjSuAlv9XbqAFRd4vUaaobuOYOOwqOBGfm0q0tyxeKDwkZK0XRg/exec" +
+                "?q=" + URLEncoder.encode(text, "UTF-8") +
+                "&target=" + langTo +
+                "&source=" + langFrom;
+        URL url = new URL(urlStr);
+        StringBuilder response = new StringBuilder();
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        return response.toString();
+    }
+
+}
